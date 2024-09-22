48c856c4f4d51043c78d7980db42c0586c6f47b0
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-09-22 14:41:34 +0800

    completed part 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,7 +8,12 @@ const Header = ({ text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>{ text } { value }</div>
+    <tbody>
+      <tr>
+        <td>{ text }</td> 
+        <td>{ value }</td>
+      </tr>
+    </tbody>
   )
 }
 
@@ -37,14 +42,14 @@ const Statistics = (props) => {
     )
   } else {
     return (
-      <div>
+      <table>
         <StatisticLine text='good' value={props.good} />
         <StatisticLine text='neutral' value={props.neutral} />
         <StatisticLine text='bad' value={props.bad} />
         <StatisticLine text='all' value={props.all} />
         <StatisticLine text='average' value={props.average} />
         <StatisticLine text='positive' value={`${props.positive} %`}/>
-      </div>
+      </table>
     )
   }
 }
