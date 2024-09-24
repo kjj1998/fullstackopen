c5698d44d111b8e0b78ab4708aeb75718916a411
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-09-24 22:05:52 +0800

    completed part 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
-  const [newName, setNewName] = useState('a new name...')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     const duplicate = persons.find((person) => person.name === newName)
@@ -19,12 +21,12 @@ const App = () => {
     } else {
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   return (
     <div>
@@ -34,13 +36,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.length === 0 
         ? '...' 
-        : persons.map(person => <div key={person.name}>{person.name}</div>)}
+        : persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
