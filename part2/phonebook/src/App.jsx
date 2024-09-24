dca27cd8113d786278e635fd40439fca2aa8f206
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-09-24 22:25:37 +0800

    completed part 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -27,10 +31,19 @@ const App = () => {
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleTermChange = (event) => {
+    setSearchTerm(event.target.value)
+
+
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchTerm} onChange={handleTermChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -45,7 +58,13 @@ const App = () => {
       <h2>Numbers</h2>
       {persons.length === 0 
         ? '...' 
-        : persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+        : persons.map(person => {
+          if (person.name.includes(searchTerm) || searchTerm === '') {
+            return (
+              <div key={person.id}>{person.name} {person.number}</div>
+            )
+          }
+      })}
     </div>
   )
 }
