86576739d699b1c4dc32d1bdc247db04b5e63b37
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-10-01 22:39:15 +0800

    added option to use the PORT env variable for phonebook backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -121,7 +121,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3002
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
