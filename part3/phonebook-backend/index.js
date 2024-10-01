9f4b530500e05de112ae1823dfc7ace9f6415a8f
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-10-01 22:32:08 +0800

    completed part 3.9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 morgan.token('body', res => JSON.stringify(res.body))
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
