eda3e940089b79d3d1fb19b590365a50c2e17b29
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-10-01 23:05:36 +0800

    uibuild-for-phonebook

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 morgan.token('body', res => JSON.stringify(res.body))
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
