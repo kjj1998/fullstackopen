2b605530590b25d1511306fabed81b289b13ac1f
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-10-05 18:32:09 +0800

    completed part 4.4

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -1,7 +1,17 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce(
+    (accumulatedLikes, blog) => {
+      return accumulatedLikes + blog.likes
+    },0
+  )
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
