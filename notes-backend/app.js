df1bca5c44afedc7e2f99aabd428c8eac33a28ab
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-10-10 21:34:05 +0800

    updated notes-backend with user authentication

--- notes-backend/app.js
+++ notes-backend/app.js
@@ -5,6 +5,8 @@ const app = express()
 const cors = require('cors')
 const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/note')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -27,6 +29,8 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
