c99ffb9e5bff23f08f0d523ae3f23497b328b2be
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-10-10 22:00:19 +0800

    completed part 4.15

--- part4/blog-backend/app.js
+++ part4/blog-backend/app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/user')
 
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -26,6 +27,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
