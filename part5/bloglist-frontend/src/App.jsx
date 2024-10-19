a0d8f6d87f4db9cc0e258c0824af044866ceb5d9
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-10-19 15:21:14 +0800

    completed part 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -15,12 +15,21 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
     try {
       const user = await loginService.login({ username, password })
       setUser(user)
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
       setUsername('')
       setPassword('')
     } catch (exception) {
@@ -28,19 +37,32 @@ const App = () => {
     }
   }
 
+  const handleLogout = (event) => {
+    event.preventDefault()
+
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogAppUser')
+  }
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          username <input type='text' value={username} name='Username' onChange={({ target }) => setUsername(target.value)}/>
+        </div>
+        <div>
+          password <input type='password' value={password} name='Password' onChange={({ target }) => setPassword(target.value)}/>
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    )
+  }
+
   if (!user) {
     return (
       <div>
         <h2>log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username <input type='text' value={username} name='Username' onChange={({ target }) => setUsername(target.value)}/>
-          </div>
-          <div>
-            password <input type='password' value={password} name='Password' onChange={({ target }) => setPassword(target.value)}/>
-          </div>
-          <button type='submit'>login</button>
-        </form>
+        {loginForm()}
       </div>
     )
   }
@@ -48,7 +70,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
