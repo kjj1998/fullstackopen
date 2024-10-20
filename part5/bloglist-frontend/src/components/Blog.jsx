bed46989f59228c1c2bd8e4f6dd8efc09de0000e
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-10-20 23:03:58 +0800

    completed part 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [display, setDisplay] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  const toggleDisplay = () => {
+    setDisplay(!display)
+  }
+
+  const displayAllDetails = () => {
+    if (display) {
+      return (
+        <div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes} <button>like</button></div>
+          <div>{blog.user.name}</div>
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleDisplay}>{display ? 'hide' : 'view'}</button>
+      {displayAllDetails()}
+    </div>  
+  )
+}
 
 export default Blog
