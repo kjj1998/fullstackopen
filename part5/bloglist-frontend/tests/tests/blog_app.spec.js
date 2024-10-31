55ef0595d4cfd1393bdadc97b963ec46e4a308e9
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-10-31 14:38:02 +0800

    completed part 5.18

--- part5/bloglist-frontend/tests/tests/blog_app.spec.js
+++ part5/bloglist-frontend/tests/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:5173/api/testing/reset')
+    await request.post('http://localhost:5173/api/users', {
+      data: {
+        username: 'hellas',
+        name: 'Artos Hellas',
+        password: 'secret'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -9,5 +18,23 @@ describe('Blog app', () => {
     await expect(page.getByTestId('username')).toBeVisible()
     await expect(page.getByTestId('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('hellas')
+      await page.getByTestId('password').fill('secret')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('logged in as Artos Hellas')).toBeVisible()
+    })
+
+    test('fails with incorrect credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('jcena')
+      await page.getByTestId('password').fill('secret')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Wrong credentials')).toBeVisible()
+    })
   })
 })
