b6c774a56966d0a12b917d1f0b173391a85e2a82
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-11-01 22:11:31 +0800

    refactored redux-notes

---
+++ redux-notes/src/App.jsx
@@ -0,0 +1,14 @@
+import NewNote from "./NewNote"
+import Notes from "./Notes"
+
+const App = () => {
+
+  return (
+    <div>
+      <NewNote />
+      <Notes />
+    </div>
+  )
+}
+
+export default App
---
+++ redux-notes/src/Notes.jsx
@@ -0,0 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleImportanceOf } from '../reducers/noteReducer'
+import PropTypes from 'prop-types'
+
+const Note = ({ note, handleClick }) => {
+  return(
+    <li onClick={handleClick}>
+      {note.content} 
+      <strong> {note.important ? 'important' : ''}</strong>
+    </li>
+  )
+}
+
+const Notes = () => {
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state)
+
+  return(
+    <ul>
+      {notes.map(note =>
+        <Note
+          key={note.id}
+          note={note}
+          handleClick={() => 
+            dispatch(toggleImportanceOf(note.id))
+          }
+        />
+      )}
+    </ul>
+  )
+}
+
+Note.propTypes = {
+  note: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired
+}
+
+export default Notes
