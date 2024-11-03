bd68f1f685501e13783ca822f8dc69511a62a439
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-11-03 14:20:07 +0800

    switched to redux-toolkit

--- redux-notes/src/main.jsx
+++ redux-notes/src/main.jsx
@@ -1,18 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import App from './App.jsx'
 import noteReducer from './reducers/noteReducer.js'
 import filterReducer from './reducers/filterReducer.js'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
