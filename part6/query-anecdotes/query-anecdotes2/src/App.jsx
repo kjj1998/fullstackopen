2637f2214b958f757379d788ce4cf7474a43f984
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-11-08 22:56:49 +0800

    completed part 6.21

---
+++ part6/query-anecdotes/query-anecdotes2/src/App.jsx
@@ -0,0 +1,50 @@
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query'
+import { getAnecdotes, createAnecdote } from './requests'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+  const queryClient = useQueryClient()
+  
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
+    retry: 2
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ part6/query-anecdotes/query-anecdotes2/src/requests.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAnecdotes = async () => {
+  const res = await axios.get(baseUrl)
+  return res.data
+}
+
+export const createAnecdote = async (anecdote) => {
+  const res = await axios.post(baseUrl, anecdote)
+  return res.data
+}
