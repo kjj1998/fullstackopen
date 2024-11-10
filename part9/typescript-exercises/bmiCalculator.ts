3c0d2050ddabc44d8fa32c5e2b7e1c10acc21f8e
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-11-10 17:09:35 +0800

    completed part 9.1

---
+++ part9/typescript-exercises/bmiCalculator.ts
@@ -0,0 +1,16 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const heightInMetres = height / 100
+  const bmi = weight / Math.pow(heightInMetres, 2)
+
+  if (bmi < 18.5) {
+    return 'Underweight range'
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return 'Normal range'
+  } else if (bmi >= 25 && bmi < 30) {
+    return 'Overweight range'
+  } else if (bmi > 30) {
+    return 'Obese range'
+  }
+}
+
+console.log(calculateBmi(180, 74))
