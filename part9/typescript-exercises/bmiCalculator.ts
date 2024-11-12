b71159d1d1f676d8c106b0f1f68b03e2a89f3f2f
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-11-12 21:35:29 +0800

    completed part 9.5

--- part9/typescript-exercises/bmiCalculator.ts
+++ part9/typescript-exercises/bmiCalculator.ts
@@ -14,36 +14,40 @@ export const parseArguments = (args: string[]): BmiValues => {
     return {
       height: Number(args[2]),
       weight: Number(args[3])
-    }
+    };
   } else {
-    throw new Error('Provided values were not numbers')
+    throw new Error('Provided values were not numbers');
   }
 }
 
 const calculateBmi = (height: number, weight: number): string => {
   if (height < 0 || weight < 0)
-    return 'Invalid weight/height given!'
+    return 'Invalid weight/height given!';
 
-  const heightInMetres = height / 100
-  const bmi = weight / Math.pow(heightInMetres, 2)
+  const heightInMetres = height / 100;
+  const bmi = weight / Math.pow(heightInMetres, 2);
 
   if (bmi < 18.5)
-    return 'Underweight range'
+    return 'Underweight range';
   else if (bmi >= 18.5 && bmi < 25)
-    return 'Normal range'
+    return 'Normal range';
   else if (bmi >= 25 && bmi < 30)
-    return 'Overweight range'
+    return 'Overweight range';
   
-  return 'Obese range'
+  return 'Obese range';
 }
 
-try {
-  const { height, weight } = parseArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.';
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
+if (require.main === module) {
+  try {
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+  } catch (error: unknown) {
+    let errorMessage = 'Something bad happened.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
+
+export default calculateBmi;
--- part9/typescript-exercises/index.ts
+++ part9/typescript-exercises/index.ts
@@ -1,8 +1,41 @@
 import express from 'express';
-const app = express();
+import { Express, Request, Response } from 'express';
+import calculateBmi from './bmiCalculator';
+
+interface bmiInformationRequest extends Request {
+  query: {
+    height: string,
+    weight: string
+  }
+};
+
+const app: Express = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req: bmiInformationRequest, res: Response) => {
+  if (!req.query.height || !req.query.weight) {
+    res.status(400).json({ error: 'Missing query parameters' });
+    return;
+  }
+
+  if (isNaN(Number(req.query.height)) || isNaN(Number(req.query.weight))) {
+    res.status(400).json({ error: 'Non numerical query parameters given' });
+    return;
+  }
+  
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  const bmiRange = calculateBmi(height, weight);
+
+  res.send({
+    weight: req.query.weight,
+    height: req.query.height,
+    bmi: bmiRange
+  });
 });
 
 const PORT = 3003;
