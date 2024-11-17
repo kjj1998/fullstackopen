f4f7d677d4ff6df5d77ce3ec9cdb7222e55baef3
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-11-17 17:11:44 +0800

    added cors to flight-diaries

--- flight-diaries/src/index.ts
+++ flight-diaries/src/index.ts
@@ -1,16 +1,23 @@
 import express from 'express';
 import diaryRouter from './routes/diaries';
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
 
 const PORT = 3000;
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200
+};
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
 
-app.use('/api/diaries', diaryRouter);
+app.use('/api/diaries', cors(corsOptions) ,diaryRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
